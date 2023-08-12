7678c88c0f7e7f21e9bb801490db5a1369ffa664
oscar.galvez <gcoscar99@yahoo.com>
2023-08-11 18:11:28 -0700

    primer commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name:'dev',
+  firebase:{
+    config : {
+
+      apiKey: "",
+
+      authDomain: "inmuebles-storage.firebaseapp.com",
+
+      projectId: "inmuebles-storage",
+
+      storageBucket: "inmuebles-storage.appspot.com",
+
+      messagingSenderId: "483272877016",
+
+      appId: "1:483272877016:web:79183e59622f8a7e3748ad"
+
+    }
+  }
 };
 
 /*
